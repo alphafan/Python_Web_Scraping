ed974d169852c5c4b324c475124dd4e351623a7a
Yufan Zheng <yufan.zheng1@gmail.com>
2018-03-17 19:19:04 +0800

    download image

---
+++ scrape_national_geographic/download_image.py
@@ -0,0 +1,21 @@
+# Download the Images of Natural Geographic
+
+import requests
+from bs4 import BeautifulSoup
+
+URL = "http://www.nationalgeographic.com.cn/animals/"
+
+html = requests.get(URL).text
+soup = BeautifulSoup(html, 'lxml')
+img_ul = soup.find_all('ul', {"class": "img_list"})
+
+for ul in img_ul:
+    imgs = ul.find_all('img')
+    for img in imgs:
+        url = img['src']
+        r = requests.get(url, stream=True)
+        image_name = url.split('/')[-1]
+        with open('./img/%s' % image_name, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=128):
+                f.write(chunk)
+        print('Saved %s' % image_name)
