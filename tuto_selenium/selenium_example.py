98c555480c35b6fb91674d2f14897690780b1339
Yufan Zheng <yufan.zheng1@gmail.com>
2018-03-18 19:21:05 +0800

    update

--- tuto_selenium/selenium_example.py
+++ tuto_selenium/selenium_example.py
@@ -1,6 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-driver = webdriver.Chrome()     # 打开 Chrome 浏览器
+# define headless
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+
+driver = webdriver.Chrome(chrome_options=chrome_options)
 
 # 将刚刚复制的帖在这
 driver.get("https://morvanzhou.github.io/")
